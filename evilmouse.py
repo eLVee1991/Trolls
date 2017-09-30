1c6fb120b4559d07481f16338caf12f6f2a17f6c
eLVee1991 <elvee@protonmail.com>
2017-09-30 02:05:04 +0200

    Created evilmouse.py

---
+++ evilmouse.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env/ python3
+
+import pyautogui as p
+from itertools import repeat
+import random
+
+p.FAILSAFE = True # With the Failsafe set to 'True' the upper left corner will stop running the code. Set it to False if you don't want to have it enabled.
+
+
+number = random.randrange(1000, 10000, 1)			# Created a random number between 1000 and 10000										# Same here
+
+for runs in repeat(None, number):					# Looping through the number of times. Using the reapeat function to specify how many times.
+	count1 = random.randrange(1, 1440, 1)			# Creating random X to move on display
+	count2 = random.randrange(335, 1335, 1)			# Creating random Y to move on display
+	p.moveTo(count1, count2)						# Moves to the coordinates
+
+p.alert("You have been trolled "+number+" times!. Thank you for your patience.")
