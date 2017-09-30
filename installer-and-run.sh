94784ab812ad25840c3da77e20e765d2593dc1a0
eLVee1991 <elvee@protonmail.com>
2017-09-30 02:35:24 +0200

    updated the the script
    Updated with: to install, run and delete after running.

--- installer-and-run.sh
+++ installer-and-run.sh
@@ -2,10 +2,18 @@
 
 echo "Fuck your life. You will be running evilmouse in a minny"
 cd
-cd "/Desktop"
+cd "$HOME/Desktop"
 mkdir evilmouse
+cd evilmouse
 wget https://raw.githubusercontent.com/eLVee1991/Trolls/master/evilmouse.py
+wget https://raw.githubusercontent.com/eLVee1991/Trolls/master/requirements.txt
 virtualenv venv
 . venv/bin/activate
-pip3 install -R requirements.txt
+pip3 install -r requirements.txt
 python3 evilmouse.py
+/Desktop"
+rm -rf evilmouse
+printf "-"*60 "/n"
+echo "You have been trolled."
+echo "Done pranking and uninstalling the script and removing everything else."
+printf "-"*60 "/n"
